e956a6204f2473050f63d6ecf432837f376ff1c6
NandikaSahu0711 <79842952+NandikaSahu0711@users.noreply.github.com>
2021-08-08 14:59:27 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,21 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBT9AVZgOWeBaR3blnxYV7GLYvKXwdkMDU",
+    authDomain: "lets-chat-53cfc.firebaseapp.com",
+    projectId: "lets-chat-53cfc",
+    storageBucket: "lets-chat-53cfc.appspot.com",
+    messagingSenderId: "239946102774",
+    appId: "1:239946102774:web:2a3ea3d887ce3b53a8f9d7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name")
+document.getElementById("user_name").innerHTML = "Welcome  " + user_name;
+
+  function getData() {firebase.database().ref("/").on('value', 
+function(snapshot) {document.getElementById("output").innerHTML = "";
+snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+Room_names = childKey;
+});});}
+getData();
